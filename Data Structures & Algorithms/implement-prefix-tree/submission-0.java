c19d5fb167a5180dfe89211598f9e05e6d330426
Aakash Chandra <142911902+Aakash50909@users.noreply.github.com>
2026-06-11 13:48:14 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,49 @@
+class TrieNode
+{
+    TrieNode children[]=new TrieNode[26];
+    boolean isEnd=false;
+}
+class PrefixTree 
+{
+             TrieNode root= new TrieNode();
+
+    public PrefixTree() 
+    {
+    }
+    public void insert(String word) 
+    {
+        TrieNode curr=root;
+        for(char c: word.toCharArray())
+        {
+            int i=c-'a';
+            if(curr.children[i]==null)
+            curr.children[i]=new TrieNode();
+            curr=curr.children[i];
+        }
+        curr.isEnd=true;
+    }
+
+    public boolean search(String word) 
+    {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (curr.children[i] == null) return false;
+            curr = curr.children[i];
+        }
+        return curr.isEnd;
+
+    }
+
+    public boolean startsWith(String prefix) 
+    {
+        TrieNode curr = root;
+        for (char c : prefix.toCharArray()) {
+            int i = c - 'a';
+            if (curr.children[i] == null) return false;
+            curr = curr.children[i];
+        }
+        return true;
+
+    }
+}
